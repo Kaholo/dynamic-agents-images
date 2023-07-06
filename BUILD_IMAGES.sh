6e1ecbe9a3fcb89675223391ed5186ace24ba38b
tomjohntaylor <46534506+tomjohntaylor@users.noreply.github.com>
2023-07-06 12:43:54 +0200

    DO-605 (Create dynamic agents images) (#1)
    * BUILD_IMAGE.sh script init
    
    * Parallelized build
    
    * fix

---
+++ BUILD_IMAGES.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+set -o pipefail # Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value
+
+AGENT_REPO_BASE_PATH=$1
+IMAGE_TAG=$2
+PUSH=$3
+
+for filename in dockerfiles/Dockerfile*; do
+    IMAGE_REPO_TAG="matankaholo/agent-${filename#"dockerfiles/Dockerfile-"}:${IMAGE_TAG}"
+    echo ${IMAGE_REPO_TAG,,}
+    docker build -f $filename -t ${IMAGE_REPO_TAG,,} ${AGENT_REPO_BASE_PATH} &
+done
+
+while true; do echo "Waiting for all containers build to finish"; ! ps -afe | grep "[d]ocker build -f dockerfiles" > /dev/null && break; sleep 30; done
+
+if [[ "$PUSH" == "push" ]]
+then
+    for filename in dockerfiles/Dockerfile*; do
+        IMAGE_REPO_TAG="matankaholo/agent-${filename#"dockerfiles/Dockerfile-"}:${IMAGE_TAG}"
+        docker push ${IMAGE_REPO_TAG,,}
+    done
+fi
